2258979036c9b847b2a12cd6ccb164e28ebb9553
Frank <frank@mdigiti.de>
2017-04-25 19:35:27 +0200

    -update copyright

--- src/yass/YassStringTransferHandler.java
+++ src/yass/YassStringTransferHandler.java
@@ -1,6 +1,6 @@
 /*
  * Yass - Karaoke Editor
- * Copyright (C) 2014 Saruta
+ * Copyright (C) 2009 Saruta
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
